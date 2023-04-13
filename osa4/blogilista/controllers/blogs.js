5e8ffa9ebfe88b6c149c67bce84419ebf207511d
Markus Becks <markus.becks@gmail.com>
2023-04-13 15:20:29 +0300

    PUT and DELETE routes

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,27 +1,50 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
+});
 
-blogsRouter.post('/', (req, res, next) => {
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogsRouter.post('/', async (req, res) => {
+  const body = req.body;
+
   const blog = new Blog({
-    title: req.body.title,
-    author: req.body.author,
-    url: req.body.url,
-    likes: req.body.likes
-  })
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  });
 
-  blog.save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-    .catch(error => next(error));
-})
+  const savedBlog = await blog.save();
+  res.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+blogsRouter.put('/:id', async (req, res) => {
+  const body = req.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true });
+  res.json(updatedBlog);
+});
 
 module.exports = blogsRouter;
